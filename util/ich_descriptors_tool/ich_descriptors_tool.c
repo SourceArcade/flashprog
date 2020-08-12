4f29bb799b4672eebcc0bdfc6fb544b2b5544b6f
Matt DeVillier <matt.devillier@gmail.com>
2020-08-12 12:48:06 -0500

    Add support for Comet Lake-U/400-series PCH
    Add enum CHIPSET_400_SERIES_COMET_POINT and treat it identically
    to CHIPSET_300_SERIES_CANNON_POINT.
    
    Add PCI IDs for Comet Lake, CML-U Premium and classify as CHIPSET_400_SERIES_COMET_POINT.
    
    Test: read/write unlocked CML-U board
    
    flashrom-stable:
    As suggested above, treat it the same as 300 series. But don't add a
    new enum.
    
    Change-Id: I43b4ad1eecfed16fec59863e46d4e997fbe45f1b
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
    Original-Reviewed-on: https://review.coreboot.org/c/flashrom/+/44420
    Original-Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Original-Reviewed-by:  Felix Singer <felixsinger@posteo.net>
    Reviewed-on: https://review.coreboot.org/c/flashrom-stable/+/71325
    Reviewed-by: Nico Huber <nico.h@gmx.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

--- util/ich_descriptors_tool/ich_descriptors_tool.c
+++ util/ich_descriptors_tool/ich_descriptors_tool.c
@@ -134,6 +134,7 @@ static void usage(char *argv[], const char *error)
 "\t- \"9\" or \"wildcat\" for Intel's 9 series chipsets.\n"
 "\t- \"100\" or \"sunrise\" for Intel's 100 series chipsets.\n"
 "\t- \"300\" or \"cannon\" for Intel's 300 series chipsets.\n"
+"\t- \"400\" or \"comet\" for Intel's 400 series chipsets.\n"
 "If '-d' is specified some regions such as the BIOS image as seen by the CPU or\n"
 "the GbE blob that is required to initialize the GbE are also dumped to files.\n",
 	argv[0], argv[0]);
@@ -223,7 +224,9 @@ int main(int argc, char *argv[])
 			 (strcmp(csn, "sunrise") == 0))
 			cs = CHIPSET_100_SERIES_SUNRISE_POINT;
 		else if ((strcmp(csn, "300") == 0) ||
-			 (strcmp(csn, "cannon") == 0))
+			 (strcmp(csn, "cannon") == 0) ||
+			 (strcmp(csn, "400") == 0) ||
+			 (strcmp(csn, "comet") == 0))
 			cs = CHIPSET_300_SERIES_CANNON_POINT;
 		else if (strcmp(csn, "apollo") == 0)
 			cs = CHIPSET_APOLLO_LAKE;
