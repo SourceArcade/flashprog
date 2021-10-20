c6c3f28c66e0ad792274ca05029a120925e21be6
Nikolai Artemiev <nartemiev@google.com>
2021-10-20 23:34:15 +1100

    flash.h,flashchips.c: add writeprotect bit layout map to chips
    This patch adds a register bit map `struct reg_bit_info`, with fields
    for storing the register, bit index, and writability of each bit that
    affects the chip's write protection. This allows writeprotect code to be
    independent of the register layout of any specific chip. The new fields
    have been filled out for example chips.
    
    The representation is centered around describing how bits can be
    accessed and modified, rather than the layout of registers. This is
    generally easier to work with in code that needs to access specific bits
    and typically requires specifying the locations of fewer bits overall.
    
    Tested: flashrom --wp-{enable,disable,range,list,status} at end of patch series
    
    Change-Id: Id08d77e6d4ca5109c0d698271146d026dbc21284
    Signed-off-by: Nikolai Artemiev <nartemiev@google.com>
    Original-Reviewed-on: https://review.coreboot.org/c/flashrom/+/58477
    Original-Reviewed-by: Nico Huber <nico.h@gmx.de>
    Original-Reviewed-by: Anastasia Klimchuk <aklm@chromium.org>
    Reviewed-on: https://review.coreboot.org/c/flashrom-stable/+/70966
    Reviewed-by: Nico Huber <nico.h@gmx.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ writeprotect.h
@@ -0,0 +1,23 @@
+/*
+ * This file is part of the flashrom project.
+ *
+ * Copyright (C) 2010 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __WRITEPROTECT_H__
+#define __WRITEPROTECT_H__ 1
+
+#define MAX_BP_BITS 4
+
+#endif /* !__WRITEPROTECT_H__ */
