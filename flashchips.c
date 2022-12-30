flashrom/flashrom

flashchips.c: Indent definition of W25Q512NW-IM properly
Signed-off-by: Felix Singer <felixsinger@posteo.net>
Change-Id: Icfd2a49383da0f8f0a4e3295aba81ce1d200652c
Reviewed-on: https://review.coreboot.org/c/flashrom/+/68151
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>

flashchips.c
@@ -18667,46 +18667,46 @@ const struct flashchip flashchips[] = {
 	},
 
 	{
-	.vendor         = "Winbond",
-	.name           = "W25Q512NW-IM",
-	.bustype        = BUS_SPI,
-	.manufacture_id = WINBOND_NEX_ID,
-	.model_id       = WINBOND_NEX_W25Q512NW_IM,
-	.total_size     = 64 * 1024,
-	.page_size      = 256,
-	.feature_bits   = FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_4BA,
-	.tested         = TEST_OK_PREW,
-	.probe          = PROBE_SPI_RDID,
-	.probe_timing   = TIMING_ZERO,
-	.block_erasers  =
-	{
+		.vendor         = "Winbond",
+		.name           = "W25Q512NW-IM",
+		.bustype        = BUS_SPI,
+		.manufacture_id = WINBOND_NEX_ID,
+		.model_id       = WINBOND_NEX_W25Q512NW_IM,
+		.total_size     = 64 * 1024,
+		.page_size      = 256,
+		.feature_bits   = FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_4BA,
+		.tested         = TEST_OK_PREW,
+		.probe          = PROBE_SPI_RDID,
+		.probe_timing   = TIMING_ZERO,
+		.block_erasers  =
 		{
-			.eraseblocks = { {4 * 1024, 16384} },
-			.block_erase = SPI_BLOCK_ERASE_21,
-		}, {
-			.eraseblocks = { {4 * 1024, 16384} },
-			.block_erase = SPI_BLOCK_ERASE_20,
-		}, {
-			.eraseblocks = { {32 * 1024, 2048} },
-			.block_erase = SPI_BLOCK_ERASE_52,
-		}, {
-			.eraseblocks = { {64 * 1024, 1024} },
-			.block_erase = SPI_BLOCK_ERASE_DC,
-		}, {
-			.eraseblocks = { {64 * 1024, 1024} },
-			.block_erase = SPI_BLOCK_ERASE_D8,
-		}, {
-			.eraseblocks = { {64 * 1024 * 1024, 1} },
-			.block_erase = SPI_BLOCK_ERASE_60,
-		}, {
-			.eraseblocks = { {64 * 1024 * 1024, 1} },
-			.block_erase = SPI_BLOCK_ERASE_C7,
-		}
-	},
-	.unlock         = SPI_DISABLE_BLOCKPROTECT,
-	.write          = SPI_CHIP_WRITE256,
-	.read           = SPI_CHIP_READ,
-	.voltage        = {1650, 1950},
+			{
+				.eraseblocks = { {4 * 1024, 16384} },
+				.block_erase = SPI_BLOCK_ERASE_21,
+			}, {
+				.eraseblocks = { {4 * 1024, 16384} },
+				.block_erase = SPI_BLOCK_ERASE_20,
+			}, {
+				.eraseblocks = { {32 * 1024, 2048} },
+				.block_erase = SPI_BLOCK_ERASE_52,
+			}, {
+				.eraseblocks = { {64 * 1024, 1024} },
+				.block_erase = SPI_BLOCK_ERASE_DC,
+			}, {
+				.eraseblocks = { {64 * 1024, 1024} },
+				.block_erase = SPI_BLOCK_ERASE_D8,
+			}, {
+				.eraseblocks = { {64 * 1024 * 1024, 1} },
+				.block_erase = SPI_BLOCK_ERASE_60,
+			}, {
+				.eraseblocks = { {64 * 1024 * 1024, 1} },
+				.block_erase = SPI_BLOCK_ERASE_C7,
+			}
+		},
+		.unlock         = SPI_DISABLE_BLOCKPROTECT,
+		.write          = SPI_CHIP_WRITE256,
+		.read           = SPI_CHIP_READ,
+		.voltage        = {1650, 1950},
 	},
 
 	{

SourceArcade/flashprog

Original-Reviewed-on: https://review.coreboot.org/c/flashrom/+/68151
Original-Reviewed-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
Reviewed-on: https://review.coreboot.org/c/flashrom-stable/+/71238
Reviewed-by: Nico Huber <nico.h@gmx.de>

flashchips.c
@@ -18321,46 +18321,46 @@ const struct flashchip flashchips[] = {
 	},
 
 	{
-	.vendor         = "Winbond",
-	.name           = "W25Q512NW-IM",
-	.bustype        = BUS_SPI,
-	.manufacture_id = WINBOND_NEX_ID,
-	.model_id       = WINBOND_NEX_W25Q512NW_IM,
-	.total_size     = 64 * 1024,
-	.page_size      = 256,
-	.feature_bits   = FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_4BA,
-	.tested         = TEST_OK_PREW,
-	.probe          = probe_spi_rdid,
-	.probe_timing   = TIMING_ZERO,
-	.block_erasers  =
-	{
+		.vendor         = "Winbond",
+		.name           = "W25Q512NW-IM",
+		.bustype        = BUS_SPI,
+		.manufacture_id = WINBOND_NEX_ID,
+		.model_id       = WINBOND_NEX_W25Q512NW_IM,
+		.total_size     = 64 * 1024,
+		.page_size      = 256,
+		.feature_bits   = FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_4BA,
+		.tested         = TEST_OK_PREW,
+		.probe          = probe_spi_rdid,
+		.probe_timing   = TIMING_ZERO,
+		.block_erasers  =
 		{
-			.eraseblocks = { {4 * 1024, 16384} },
-			.block_erase = spi_block_erase_21,
-		}, {
-			.eraseblocks = { {4 * 1024, 16384} },
-			.block_erase = spi_block_erase_20,
-		}, {
-			.eraseblocks = { {32 * 1024, 2048} },
-			.block_erase = spi_block_erase_52,
-		}, {
-			.eraseblocks = { {64 * 1024, 1024} },
-			.block_erase = spi_block_erase_dc,
-		}, {
-			.eraseblocks = { {64 * 1024, 1024} },
-			.block_erase = spi_block_erase_d8,
-		}, {
-			.eraseblocks = { {64 * 1024 * 1024, 1} },
-			.block_erase = spi_block_erase_60,
-		}, {
-			.eraseblocks = { {64 * 1024 * 1024, 1} },
-			.block_erase = spi_block_erase_c7,
-		}
-	},
-	.unlock         = spi_disable_blockprotect,
-	.write          = spi_chip_write_256,
-	.read           = spi_chip_read,
-	.voltage        = {1650, 1950},
+			{
+				.eraseblocks = { {4 * 1024, 16384} },
+				.block_erase = spi_block_erase_21,
+			}, {
+				.eraseblocks = { {4 * 1024, 16384} },
+				.block_erase = spi_block_erase_20,
+			}, {
+				.eraseblocks = { {32 * 1024, 2048} },
+				.block_erase = spi_block_erase_52,
+			}, {
+				.eraseblocks = { {64 * 1024, 1024} },
+				.block_erase = spi_block_erase_dc,
+			}, {
+				.eraseblocks = { {64 * 1024, 1024} },
+				.block_erase = spi_block_erase_d8,
+			}, {
+				.eraseblocks = { {64 * 1024 * 1024, 1} },
+				.block_erase = spi_block_erase_60,
+			}, {
+				.eraseblocks = { {64 * 1024 * 1024, 1} },
+				.block_erase = spi_block_erase_c7,
+			}
+		},
+		.unlock         = spi_disable_blockprotect,
+		.write          = spi_chip_write_256,
+		.read           = spi_chip_read,
+		.voltage        = {1650, 1950},
 	},
 
 	{