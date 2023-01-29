flashrom/flashrom

Makefile: rename FEATURE_CFLAGS to FEATURE_FLAGS
Change-Id: I819f5d76e6f37a0e0ed6481a051ed85126622503
Signed-off-by: Thomas Heijligen <thomas.heijligen@secunet.com>
Reviewed-on: https://review.coreboot.org/c/flashrom/+/59231
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Nico Huber <nico.h@gmx.de>

Makefile
@@ -268,9 +268,9 @@ $(call mark_unsupported,$(DEPENDS_ON_SERIAL))
 endif
 
 ifeq ($(TARGET_OS), $(filter $(TARGET_OS), MinGW Cygwin))
-FEATURE_CFLAGS += -D'IS_WINDOWS=1'
+FEATURE_FLAGS += -D'IS_WINDOWS=1'
 else
-FEATURE_CFLAGS += -D'IS_WINDOWS=0'
+FEATURE_FLAGS += -D'IS_WINDOWS=0'
 endif
 
 # FIXME: Should we check for Cygwin/MSVC as well?
@@ -352,10 +352,10 @@ $(call mark_unsupported,$(DEPENDS_ON_LIBUSB1))
 endif
 
 ifeq ($(ENDIAN), little)
-FEATURE_CFLAGS += -D'__FLASHROM_LITTLE_ENDIAN__=1'
+FEATURE_FLAGS += -D'__FLASHROM_LITTLE_ENDIAN__=1'
 endif
 ifeq ($(ENDIAN), big)
-FEATURE_CFLAGS += -D'__FLASHROM_BIG_ENDIAN__=1'
+FEATURE_FLAGS += -D'__FLASHROM_BIG_ENDIAN__=1'
 endif
 
 # PCI port I/O support is unimplemented on PPC/MIPS/SPARC and unavailable on ARM.
@@ -373,21 +373,21 @@ $(call mark_unsupported,$(DEPENDS_ON_RAW_MEM_ACCESS))
 endif
 
 ifeq ($(TARGET_OS), $(filter $(TARGET_OS), Linux Darwin NetBSD OpenBSD))
-FEATURE_CFLAGS += -D'USE_IOPL=1'
+FEATURE_FLAGS += -D'USE_IOPL=1'
 else
-FEATURE_CFLAGS += -D'USE_IOPL=0'
+FEATURE_FLAGS += -D'USE_IOPL=0'
 endif
 
 ifeq ($(TARGET_OS), $(filter $(TARGET_OS), FreeBSD FreeBSD-glibc DragonFlyBSD))
-FEATURE_CFLAGS += -D'USE_DEV_IO=1'
+FEATURE_FLAGS += -D'USE_DEV_IO=1'
 else
-FEATURE_CFLAGS += -D'USE_DEV_IO=0'
+FEATURE_FLAGS += -D'USE_DEV_IO=0'
 endif
 
 ifeq ($(TARGET_OS), $(filter $(TARGET_OS), Hurd))
-FEATURE_CFLAGS += -D'USE_IOPERM=1'
+FEATURE_FLAGS += -D'USE_IOPERM=1'
 else
-FEATURE_CFLAGS += -D'USE_IOPERM=0'
+FEATURE_FLAGS += -D'USE_IOPERM=0'
 endif
 
 
@@ -590,212 +590,212 @@ CONFIG_INTERNAL_DMI ?= yes
 # Depending on the CONFIG_* variables set and verified above we set compiler flags and parameters below.
 
 ifdef CONFIG_DEFAULT_PROGRAMMER_NAME
-FEATURE_CFLAGS += -D'CONFIG_DEFAULT_PROGRAMMER_NAME=&programmer_$(CONFIG_DEFAULT_PROGRAMMER_NAME)'
+FEATURE_FLAGS += -D'CONFIG_DEFAULT_PROGRAMMER_NAME=&programmer_$(CONFIG_DEFAULT_PROGRAMMER_NAME)'
 else
-FEATURE_CFLAGS += -D'CONFIG_DEFAULT_PROGRAMMER_NAME=NULL'
+FEATURE_FLAGS += -D'CONFIG_DEFAULT_PROGRAMMER_NAME=NULL'
 endif
 
-FEATURE_CFLAGS += -D'CONFIG_DEFAULT_PROGRAMMER_ARGS="$(CONFIG_DEFAULT_PROGRAMMER_ARGS)"'
+FEATURE_FLAGS += -D'CONFIG_DEFAULT_PROGRAMMER_ARGS="$(CONFIG_DEFAULT_PROGRAMMER_ARGS)"'
 
 ifeq ($(CONFIG_INTERNAL), yes)
-FEATURE_CFLAGS += -D'CONFIG_INTERNAL=1'
+FEATURE_FLAGS += -D'CONFIG_INTERNAL=1'
 PROGRAMMER_OBJS += processor_enable.o chipset_enable.o board_enable.o cbtable.o internal.o
 ifeq ($(ARCH), x86)
 PROGRAMMER_OBJS += it87spi.o it85spi.o sb600spi.o amd_imc.o wbsio_spi.o mcp6x_spi.o
 PROGRAMMER_OBJS += ichspi.o dmi.o
 ifeq ($(CONFIG_INTERNAL_DMI), yes)
-FEATURE_CFLAGS += -D'CONFIG_INTERNAL_DMI=1'
+FEATURE_FLAGS += -D'CONFIG_INTERNAL_DMI=1'
 endif
 else
 endif
 endif
 
 ifeq ($(CONFIG_SERPROG), yes)
-FEATURE_CFLAGS += -D'CONFIG_SERPROG=1'
+FEATURE_FLAGS += -D'CONFIG_SERPROG=1'
 PROGRAMMER_OBJS += serprog.o
 NEED_SERIAL += CONFIG_SERPROG
 NEED_POSIX_SOCKETS += CONFIG_SERPROG
 endif
 
 ifeq ($(CONFIG_RAYER_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_RAYER_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_RAYER_SPI=1'
 PROGRAMMER_OBJS += rayer_spi.o
 endif
 
 ifeq ($(CONFIG_RAIDEN_DEBUG_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_RAIDEN_DEBUG_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_RAIDEN_DEBUG_SPI=1'
 PROGRAMMER_OBJS += raiden_debug_spi.o
 endif
 
 ifeq ($(CONFIG_PONY_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_PONY_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_PONY_SPI=1'
 PROGRAMMER_OBJS += pony_spi.o
 NEED_SERIAL += CONFIG_PONY_SPI
 endif
 
 ifeq ($(CONFIG_BITBANG_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_BITBANG_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_BITBANG_SPI=1'
 PROGRAMMER_OBJS += bitbang_spi.o
 endif
 
 ifeq ($(CONFIG_NIC3COM), yes)
-FEATURE_CFLAGS += -D'CONFIG_NIC3COM=1'
+FEATURE_FLAGS += -D'CONFIG_NIC3COM=1'
 PROGRAMMER_OBJS += nic3com.o
 endif
 
 ifeq ($(CONFIG_GFXNVIDIA), yes)
-FEATURE_CFLAGS += -D'CONFIG_GFXNVIDIA=1'
+FEATURE_FLAGS += -D'CONFIG_GFXNVIDIA=1'
 PROGRAMMER_OBJS += gfxnvidia.o
 endif
 
 ifeq ($(CONFIG_SATASII), yes)
-FEATURE_CFLAGS += -D'CONFIG_SATASII=1'
+FEATURE_FLAGS += -D'CONFIG_SATASII=1'
 PROGRAMMER_OBJS += satasii.o
 endif
 
 ifeq ($(CONFIG_ATAHPT), yes)
-FEATURE_CFLAGS += -D'CONFIG_ATAHPT=1'
+FEATURE_FLAGS += -D'CONFIG_ATAHPT=1'
 PROGRAMMER_OBJS += atahpt.o
 endif
 
 ifeq ($(CONFIG_ATAVIA), yes)
-FEATURE_CFLAGS += -D'CONFIG_ATAVIA=1'
+FEATURE_FLAGS += -D'CONFIG_ATAVIA=1'
 PROGRAMMER_OBJS += atavia.o
 endif
 
 ifeq ($(CONFIG_ATAPROMISE), yes)
-FEATURE_CFLAGS += -D'CONFIG_ATAPROMISE=1'
+FEATURE_FLAGS += -D'CONFIG_ATAPROMISE=1'
 PROGRAMMER_OBJS += atapromise.o
 endif
 
 ifeq ($(CONFIG_IT8212), yes)
-FEATURE_CFLAGS += -D'CONFIG_IT8212=1'
+FEATURE_FLAGS += -D'CONFIG_IT8212=1'
 PROGRAMMER_OBJS += it8212.o
 endif
 
 ifeq ($(CONFIG_FT2232_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_FT2232_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_FT2232_SPI=1'
 PROGRAMMER_OBJS += ft2232_spi.o
 endif
 
 ifeq ($(CONFIG_USBBLASTER_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_USBBLASTER_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_USBBLASTER_SPI=1'
 PROGRAMMER_OBJS += usbblaster_spi.o
 endif
 
 ifeq ($(CONFIG_PICKIT2_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_PICKIT2_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_PICKIT2_SPI=1'
 PROGRAMMER_OBJS += pickit2_spi.o
 endif
 
 ifeq ($(CONFIG_STLINKV3_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_STLINKV3_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_STLINKV3_SPI=1'
 PROGRAMMER_OBJS += stlinkv3_spi.o
 endif
 
 ifeq ($(CONFIG_LSPCON_I2C_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_LSPCON_I2C_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_LSPCON_I2C_SPI=1'
 PROGRAMMER_OBJS += lspcon_i2c_spi.o
 endif
 
 ifeq ($(CONFIG_REALTEK_MST_I2C_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_REALTEK_MST_I2C_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_REALTEK_MST_I2C_SPI=1'
 PROGRAMMER_OBJS += realtek_mst_i2c_spi.o
 endif
 
 ifeq ($(CONFIG_DUMMY), yes)
-FEATURE_CFLAGS += -D'CONFIG_DUMMY=1'
+FEATURE_FLAGS += -D'CONFIG_DUMMY=1'
 PROGRAMMER_OBJS += dummyflasher.o
 endif
 
 ifeq ($(CONFIG_DRKAISER), yes)
-FEATURE_CFLAGS += -D'CONFIG_DRKAISER=1'
+FEATURE_FLAGS += -D'CONFIG_DRKAISER=1'
 PROGRAMMER_OBJS += drkaiser.o
 endif
 
 ifeq ($(CONFIG_NICREALTEK), yes)
-FEATURE_CFLAGS += -D'CONFIG_NICREALTEK=1'
+FEATURE_FLAGS += -D'CONFIG_NICREALTEK=1'
 PROGRAMMER_OBJS += nicrealtek.o
 endif
 
 ifeq ($(CONFIG_NICNATSEMI), yes)
-FEATURE_CFLAGS += -D'CONFIG_NICNATSEMI=1'
+FEATURE_FLAGS += -D'CONFIG_NICNATSEMI=1'
 PROGRAMMER_OBJS += nicnatsemi.o
 endif
 
 ifeq ($(CONFIG_NICINTEL), yes)
-FEATURE_CFLAGS += -D'CONFIG_NICINTEL=1'
+FEATURE_FLAGS += -D'CONFIG_NICINTEL=1'
 PROGRAMMER_OBJS += nicintel.o
 endif
 
 ifeq ($(CONFIG_NICINTEL_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_NICINTEL_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_NICINTEL_SPI=1'
 PROGRAMMER_OBJS += nicintel_spi.o
 endif
 
 ifeq ($(CONFIG_NICINTEL_EEPROM), yes)
-FEATURE_CFLAGS += -D'CONFIG_NICINTEL_EEPROM=1'
+FEATURE_FLAGS += -D'CONFIG_NICINTEL_EEPROM=1'
 PROGRAMMER_OBJS += nicintel_eeprom.o
 endif
 
 ifeq ($(CONFIG_OGP_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_OGP_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_OGP_SPI=1'
 PROGRAMMER_OBJS += ogp_spi.o
 endif
 
 ifeq ($(CONFIG_BUSPIRATE_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_BUSPIRATE_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_BUSPIRATE_SPI=1'
 PROGRAMMER_OBJS += buspirate_spi.o
 NEED_SERIAL += CONFIG_BUSPIRATE_SPI
 endif
 
 ifeq ($(CONFIG_DEDIPROG), yes)
-FEATURE_CFLAGS += -D'CONFIG_DEDIPROG=1'
+FEATURE_FLAGS += -D'CONFIG_DEDIPROG=1'
 PROGRAMMER_OBJS += dediprog.o
 endif
 
 ifeq ($(CONFIG_DEVELOPERBOX_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_DEVELOPERBOX_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_DEVELOPERBOX_SPI=1'
 PROGRAMMER_OBJS += developerbox_spi.o
 endif
 
 ifeq ($(CONFIG_SATAMV), yes)
-FEATURE_CFLAGS += -D'CONFIG_SATAMV=1'
+FEATURE_FLAGS += -D'CONFIG_SATAMV=1'
 PROGRAMMER_OBJS += satamv.o
 endif
 
 ifeq ($(CONFIG_LINUX_MTD), yes)
-FEATURE_CFLAGS += -D'CONFIG_LINUX_MTD=1'
+FEATURE_FLAGS += -D'CONFIG_LINUX_MTD=1'
 PROGRAMMER_OBJS += linux_mtd.o
 endif
 
 ifeq ($(CONFIG_LINUX_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_LINUX_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_LINUX_SPI=1'
 PROGRAMMER_OBJS += linux_spi.o
 endif
 
 ifeq ($(CONFIG_MSTARDDC_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_MSTARDDC_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_MSTARDDC_SPI=1'
 PROGRAMMER_OBJS += mstarddc_spi.o
 endif
 
 ifeq ($(CONFIG_CH341A_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_CH341A_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_CH341A_SPI=1'
 PROGRAMMER_OBJS += ch341a_spi.o
 endif
 
 ifeq ($(CONFIG_DIGILENT_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_DIGILENT_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_DIGILENT_SPI=1'
 PROGRAMMER_OBJS += digilent_spi.o
 endif
 
 ifeq ($(CONFIG_JLINK_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_JLINK_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_JLINK_SPI=1'
 PROGRAMMER_OBJS += jlink_spi.o
 endif
 
 ifeq ($(CONFIG_NI845X_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_NI845X_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_NI845X_SPI=1'
 PROGRAMMER_OBJS += ni845x_spi.o
 endif
 
@@ -825,7 +825,7 @@ endif
 
 USE_X86_PORT_IO := $(if $(call filter_deps,$(DEPENDS_ON_X86_PORT_IO)),yes,no)
 ifeq ($(USE_X86_PORT_IO), yes)
-FEATURE_CFLAGS += -D'__FLASHROM_HAVE_OUTB__=1'
+FEATURE_FLAGS += -D'__FLASHROM_HAVE_OUTB__=1'
 PROGRAMMER_OBJS += hwaccess_x86_io.o
 endif
 
@@ -849,7 +849,7 @@ endif
 USE_LIBPCI := $(if $(call filter_deps,$(DEPENDS_ON_LIBPCI)),yes,no)
 ifeq ($(USE_LIBPCI), yes)
 PROGRAMMER_OBJS += pcidev.o
-FEATURE_CFLAGS += -D'NEED_PCI=1'
+FEATURE_FLAGS += -D'NEED_PCI=1'
 override CFLAGS  += $(CONFIG_LIBPCI_CFLAGS)
 override LDFLAGS += $(CONFIG_LIBPCI_LDFLAGS)
 endif
@@ -866,7 +866,7 @@ ifeq ($(USE_LIBFTDI1), yes)
 override CFLAGS  += $(CONFIG_LIBFTDI1_CFLAGS)
 override LDFLAGS += $(CONFIG_LIBFTDI1_LDFLAGS)
 ifeq ($(HAS_FT232H), yes)
-FEATURE_CFLAGS += -D'HAVE_FT232H=1'
+FEATURE_FLAGS += -D'HAVE_FT232H=1'
 endif
 endif
 
@@ -883,16 +883,16 @@ override LDFLAGS += $(CONFIG_LIBJAYLINK_LDFLAGS)
 endif
 
 ifeq ($(CONFIG_PRINT_WIKI), yes)
-FEATURE_CFLAGS += -D'CONFIG_PRINT_WIKI=1'
+FEATURE_FLAGS += -D'CONFIG_PRINT_WIKI=1'
 CLI_OBJS += print_wiki.o
 endif
 
 ifeq ($(HAS_UTSNAME), yes)
-FEATURE_CFLAGS += -D'HAVE_UTSNAME=1'
+FEATURE_FLAGS += -D'HAVE_UTSNAME=1'
 endif
 
 ifeq ($(HAS_CLOCK_GETTIME), yes)
-FEATURE_CFLAGS += -D'HAVE_CLOCK_GETTIME=1'
+FEATURE_FLAGS += -D'HAVE_CLOCK_GETTIME=1'
 override LDFLAGS += -lrt
 endif
 
@@ -961,7 +961,7 @@ config:
 	fi
 
 %.o: %.c config
-	$(CC) -MMD $(CFLAGS) $(CPPFLAGS) $(FLASHROM_CFLAGS) $(FEATURE_CFLAGS) $(SCMDEF) -o $@ -c $<
+	$(CC) -MMD $(CFLAGS) $(CPPFLAGS) $(FLASHROM_CFLAGS) $(FEATURE_FLAGS) $(SCMDEF) -o $@ -c $<
 
 $(PROGRAM)$(EXEC_SUFFIX): $(OBJS)
 	$(CC) -o $(PROGRAM)$(EXEC_SUFFIX) $(OBJS) $(LDFLAGS)

SourceArcade/flashprog

Original-Reviewed-on: https://review.coreboot.org/c/flashrom/+/59231
Original-Reviewed-by: Nico Huber <nico.h@gmx.de>
Reviewed-on: https://review.coreboot.org/c/flashrom-stable/+/72285

Makefile
@@ -275,9 +275,9 @@ ifeq ($(TARGET_OS), $(filter $(TARGET_OS), MinGW Cygwin))
 $(call mark_unsupported,$(DEPENDS_ON_RAW_MEM_ACCESS))
 $(call mark_unsupported,$(DEPENDS_ON_X86_PORT_IO))
 $(call mark_unsupported,$(DEPENDS_ON_X86_MSR))
-FEATURE_CFLAGS += -D'IS_WINDOWS=1'
+FEATURE_FLAGS += -D'IS_WINDOWS=1'
 else
-FEATURE_CFLAGS += -D'IS_WINDOWS=0'
+FEATURE_FLAGS += -D'IS_WINDOWS=0'
 endif
 
 # FIXME: Should we check for Cygwin/MSVC as well?
@@ -357,10 +357,10 @@ $(call mark_unsupported,$(DEPENDS_ON_LIBUSB1))
 endif
 
 ifeq ($(ENDIAN), little)
-FEATURE_CFLAGS += -D'__FLASHROM_LITTLE_ENDIAN__=1'
+FEATURE_FLAGS += -D'__FLASHROM_LITTLE_ENDIAN__=1'
 endif
 ifeq ($(ENDIAN), big)
-FEATURE_CFLAGS += -D'__FLASHROM_BIG_ENDIAN__=1'
+FEATURE_FLAGS += -D'__FLASHROM_BIG_ENDIAN__=1'
 endif
 
 # PCI port I/O support is unimplemented on PPC/MIPS/SPARC and unavailable on ARM.
@@ -378,21 +378,21 @@ $(call mark_unsupported,$(DEPENDS_ON_RAW_MEM_ACCESS))
 endif
 
 ifeq ($(TARGET_OS), $(filter $(TARGET_OS), Linux Darwin NetBSD OpenBSD))
-FEATURE_CFLAGS += -D'USE_IOPL=1'
+FEATURE_FLAGS += -D'USE_IOPL=1'
 else
-FEATURE_CFLAGS += -D'USE_IOPL=0'
+FEATURE_FLAGS += -D'USE_IOPL=0'
 endif
 
 ifeq ($(TARGET_OS), $(filter $(TARGET_OS), FreeBSD FreeBSD-glibc DragonFlyBSD))
-FEATURE_CFLAGS += -D'USE_DEV_IO=1'
+FEATURE_FLAGS += -D'USE_DEV_IO=1'
 else
-FEATURE_CFLAGS += -D'USE_DEV_IO=0'
+FEATURE_FLAGS += -D'USE_DEV_IO=0'
 endif
 
 ifeq ($(TARGET_OS), $(filter $(TARGET_OS), Hurd))
-FEATURE_CFLAGS += -D'USE_IOPERM=1'
+FEATURE_FLAGS += -D'USE_IOPERM=1'
 else
-FEATURE_CFLAGS += -D'USE_IOPERM=0'
+FEATURE_FLAGS += -D'USE_IOPERM=0'
 endif
 
 
@@ -586,202 +586,202 @@ CONFIG_INTERNAL_DMI ?= yes
 # Depending on the CONFIG_* variables set and verified above we set compiler flags and parameters below.
 
 ifdef CONFIG_DEFAULT_PROGRAMMER_NAME
-FEATURE_CFLAGS += -D'CONFIG_DEFAULT_PROGRAMMER_NAME=&programmer_$(CONFIG_DEFAULT_PROGRAMMER_NAME)'
+FEATURE_FLAGS += -D'CONFIG_DEFAULT_PROGRAMMER_NAME=&programmer_$(CONFIG_DEFAULT_PROGRAMMER_NAME)'
 else
-FEATURE_CFLAGS += -D'CONFIG_DEFAULT_PROGRAMMER_NAME=NULL'
+FEATURE_FLAGS += -D'CONFIG_DEFAULT_PROGRAMMER_NAME=NULL'
 endif
 
-FEATURE_CFLAGS += -D'CONFIG_DEFAULT_PROGRAMMER_ARGS="$(CONFIG_DEFAULT_PROGRAMMER_ARGS)"'
+FEATURE_FLAGS += -D'CONFIG_DEFAULT_PROGRAMMER_ARGS="$(CONFIG_DEFAULT_PROGRAMMER_ARGS)"'
 
 ifeq ($(CONFIG_INTERNAL), yes)
-FEATURE_CFLAGS += -D'CONFIG_INTERNAL=1'
+FEATURE_FLAGS += -D'CONFIG_INTERNAL=1'
 PROGRAMMER_OBJS += processor_enable.o chipset_enable.o board_enable.o cbtable.o internal.o
 ifeq ($(ARCH), x86)
 PROGRAMMER_OBJS += it87spi.o sb600spi.o amd_imc.o wbsio_spi.o mcp6x_spi.o
 PROGRAMMER_OBJS += ichspi.o dmi.o
 ifeq ($(CONFIG_INTERNAL_DMI), yes)
-FEATURE_CFLAGS += -D'CONFIG_INTERNAL_DMI=1'
+FEATURE_FLAGS += -D'CONFIG_INTERNAL_DMI=1'
 endif
 else
 endif
 endif
 
 ifeq ($(CONFIG_SERPROG), yes)
-FEATURE_CFLAGS += -D'CONFIG_SERPROG=1'
+FEATURE_FLAGS += -D'CONFIG_SERPROG=1'
 PROGRAMMER_OBJS += serprog.o
 NEED_SERIAL += CONFIG_SERPROG
 NEED_POSIX_SOCKETS += CONFIG_SERPROG
 endif
 
 ifeq ($(CONFIG_RAYER_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_RAYER_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_RAYER_SPI=1'
 PROGRAMMER_OBJS += rayer_spi.o
 endif
 
 ifeq ($(CONFIG_PONY_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_PONY_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_PONY_SPI=1'
 PROGRAMMER_OBJS += pony_spi.o
 NEED_SERIAL += CONFIG_PONY_SPI
 endif
 
 ifeq ($(CONFIG_BITBANG_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_BITBANG_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_BITBANG_SPI=1'
 PROGRAMMER_OBJS += bitbang_spi.o
 endif
 
 ifeq ($(CONFIG_NIC3COM), yes)
-FEATURE_CFLAGS += -D'CONFIG_NIC3COM=1'
+FEATURE_FLAGS += -D'CONFIG_NIC3COM=1'
 PROGRAMMER_OBJS += nic3com.o
 endif
 
 ifeq ($(CONFIG_GFXNVIDIA), yes)
-FEATURE_CFLAGS += -D'CONFIG_GFXNVIDIA=1'
+FEATURE_FLAGS += -D'CONFIG_GFXNVIDIA=1'
 PROGRAMMER_OBJS += gfxnvidia.o
 endif
 
 ifeq ($(CONFIG_SATASII), yes)
-FEATURE_CFLAGS += -D'CONFIG_SATASII=1'
+FEATURE_FLAGS += -D'CONFIG_SATASII=1'
 PROGRAMMER_OBJS += satasii.o
 endif
 
 ifeq ($(CONFIG_ATAHPT), yes)
-FEATURE_CFLAGS += -D'CONFIG_ATAHPT=1'
+FEATURE_FLAGS += -D'CONFIG_ATAHPT=1'
 PROGRAMMER_OBJS += atahpt.o
 endif
 
 ifeq ($(CONFIG_ATAVIA), yes)
-FEATURE_CFLAGS += -D'CONFIG_ATAVIA=1'
+FEATURE_FLAGS += -D'CONFIG_ATAVIA=1'
 PROGRAMMER_OBJS += atavia.o
 endif
 
 ifeq ($(CONFIG_ATAPROMISE), yes)
-FEATURE_CFLAGS += -D'CONFIG_ATAPROMISE=1'
+FEATURE_FLAGS += -D'CONFIG_ATAPROMISE=1'
 PROGRAMMER_OBJS += atapromise.o
 endif
 
 ifeq ($(CONFIG_IT8212), yes)
-FEATURE_CFLAGS += -D'CONFIG_IT8212=1'
+FEATURE_FLAGS += -D'CONFIG_IT8212=1'
 PROGRAMMER_OBJS += it8212.o
 endif
 
 ifeq ($(CONFIG_FT2232_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_FT2232_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_FT2232_SPI=1'
 PROGRAMMER_OBJS += ft2232_spi.o
 endif
 
 ifeq ($(CONFIG_USBBLASTER_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_USBBLASTER_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_USBBLASTER_SPI=1'
 PROGRAMMER_OBJS += usbblaster_spi.o
 endif
 
 ifeq ($(CONFIG_PICKIT2_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_PICKIT2_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_PICKIT2_SPI=1'
 PROGRAMMER_OBJS += pickit2_spi.o
 endif
 
 ifeq ($(CONFIG_STLINKV3_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_STLINKV3_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_STLINKV3_SPI=1'
 PROGRAMMER_OBJS += stlinkv3_spi.o
 endif
 
 ifeq ($(CONFIG_DUMMY), yes)
-FEATURE_CFLAGS += -D'CONFIG_DUMMY=1'
+FEATURE_FLAGS += -D'CONFIG_DUMMY=1'
 PROGRAMMER_OBJS += dummyflasher.o
 endif
 
 ifeq ($(CONFIG_DRKAISER), yes)
-FEATURE_CFLAGS += -D'CONFIG_DRKAISER=1'
+FEATURE_FLAGS += -D'CONFIG_DRKAISER=1'
 PROGRAMMER_OBJS += drkaiser.o
 endif
 
 ifeq ($(CONFIG_NICREALTEK), yes)
-FEATURE_CFLAGS += -D'CONFIG_NICREALTEK=1'
+FEATURE_FLAGS += -D'CONFIG_NICREALTEK=1'
 PROGRAMMER_OBJS += nicrealtek.o
 endif
 
 ifeq ($(CONFIG_NICNATSEMI), yes)
-FEATURE_CFLAGS += -D'CONFIG_NICNATSEMI=1'
+FEATURE_FLAGS += -D'CONFIG_NICNATSEMI=1'
 PROGRAMMER_OBJS += nicnatsemi.o
 endif
 
 ifeq ($(CONFIG_NICINTEL), yes)
-FEATURE_CFLAGS += -D'CONFIG_NICINTEL=1'
+FEATURE_FLAGS += -D'CONFIG_NICINTEL=1'
 PROGRAMMER_OBJS += nicintel.o
 endif
 
 ifeq ($(CONFIG_NICINTEL_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_NICINTEL_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_NICINTEL_SPI=1'
 PROGRAMMER_OBJS += nicintel_spi.o
 endif
 
 ifeq ($(CONFIG_NICINTEL_EEPROM), yes)
-FEATURE_CFLAGS += -D'CONFIG_NICINTEL_EEPROM=1'
+FEATURE_FLAGS += -D'CONFIG_NICINTEL_EEPROM=1'
 PROGRAMMER_OBJS += nicintel_eeprom.o
 endif
 
 ifeq ($(CONFIG_OGP_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_OGP_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_OGP_SPI=1'
 PROGRAMMER_OBJS += ogp_spi.o
 endif
 
 ifeq ($(CONFIG_BUSPIRATE_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_BUSPIRATE_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_BUSPIRATE_SPI=1'
 PROGRAMMER_OBJS += buspirate_spi.o
 NEED_SERIAL += CONFIG_BUSPIRATE_SPI
 endif
 
 ifeq ($(CONFIG_DEDIPROG), yes)
-FEATURE_CFLAGS += -D'CONFIG_DEDIPROG=1'
+FEATURE_FLAGS += -D'CONFIG_DEDIPROG=1'
 PROGRAMMER_OBJS += dediprog.o
 endif
 
 ifeq ($(CONFIG_DEVELOPERBOX_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_DEVELOPERBOX_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_DEVELOPERBOX_SPI=1'
 PROGRAMMER_OBJS += developerbox_spi.o
 endif
 
 ifeq ($(CONFIG_SATAMV), yes)
-FEATURE_CFLAGS += -D'CONFIG_SATAMV=1'
+FEATURE_FLAGS += -D'CONFIG_SATAMV=1'
 PROGRAMMER_OBJS += satamv.o
 endif
 
 ifeq ($(CONFIG_LINUX_MTD), yes)
-FEATURE_CFLAGS += -D'CONFIG_LINUX_MTD=1'
+FEATURE_FLAGS += -D'CONFIG_LINUX_MTD=1'
 PROGRAMMER_OBJS += linux_mtd.o
 endif
 
 ifeq ($(CONFIG_LINUX_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_LINUX_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_LINUX_SPI=1'
 PROGRAMMER_OBJS += linux_spi.o
 endif
 
 ifeq ($(CONFIG_MSTARDDC_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_MSTARDDC_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_MSTARDDC_SPI=1'
 PROGRAMMER_OBJS += mstarddc_spi.o
 endif
 
 ifeq ($(CONFIG_CH341A_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_CH341A_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_CH341A_SPI=1'
 PROGRAMMER_OBJS += ch341a_spi.o
 endif
 
 ifeq ($(CONFIG_DIGILENT_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_DIGILENT_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_DIGILENT_SPI=1'
 PROGRAMMER_OBJS += digilent_spi.o
 endif
 
 ifeq ($(CONFIG_DIRTYJTAG_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_DIRTYJTAG_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_DIRTYJTAG_SPI=1'
 PROGRAMMER_OBJS += dirtyjtag_spi.o
 endif
 
 ifeq ($(CONFIG_JLINK_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_JLINK_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_JLINK_SPI=1'
 PROGRAMMER_OBJS += jlink_spi.o
 endif
 
 ifeq ($(CONFIG_NI845X_SPI), yes)
-FEATURE_CFLAGS += -D'CONFIG_NI845X_SPI=1'
+FEATURE_FLAGS += -D'CONFIG_NI845X_SPI=1'
 PROGRAMMER_OBJS += ni845x_spi.o
 endif
 
@@ -807,7 +807,7 @@ endif
 
 USE_X86_PORT_IO := $(if $(call filter_deps,$(DEPENDS_ON_X86_PORT_IO)),yes,no)
 ifeq ($(USE_X86_PORT_IO), yes)
-FEATURE_CFLAGS += -D'__FLASHROM_HAVE_OUTB__=1'
+FEATURE_FLAGS += -D'__FLASHROM_HAVE_OUTB__=1'
 PROGRAMMER_OBJS += hwaccess_x86_io.o
 endif
 
@@ -831,7 +831,7 @@ endif
 USE_LIBPCI := $(if $(call filter_deps,$(DEPENDS_ON_LIBPCI)),yes,no)
 ifeq ($(USE_LIBPCI), yes)
 PROGRAMMER_OBJS += pcidev.o
-FEATURE_CFLAGS += -D'NEED_PCI=1'
+FEATURE_FLAGS += -D'NEED_PCI=1'
 override CFLAGS  += $(CONFIG_LIBPCI_CFLAGS)
 override LDFLAGS += $(CONFIG_LIBPCI_LDFLAGS)
 endif
@@ -848,7 +848,7 @@ ifeq ($(USE_LIBFTDI1), yes)
 override CFLAGS  += $(CONFIG_LIBFTDI1_CFLAGS)
 override LDFLAGS += $(CONFIG_LIBFTDI1_LDFLAGS)
 ifeq ($(HAS_FT232H), yes)
-FEATURE_CFLAGS += -D'HAVE_FT232H=1'
+FEATURE_FLAGS += -D'HAVE_FT232H=1'
 endif
 endif
 
@@ -865,16 +865,16 @@ override LDFLAGS += $(CONFIG_LIBJAYLINK_LDFLAGS)
 endif
 
 ifeq ($(CONFIG_PRINT_WIKI), yes)
-FEATURE_CFLAGS += -D'CONFIG_PRINT_WIKI=1'
+FEATURE_FLAGS += -D'CONFIG_PRINT_WIKI=1'
 CLI_OBJS += print_wiki.o
 endif
 
 ifeq ($(HAS_UTSNAME), yes)
-FEATURE_CFLAGS += -D'HAVE_UTSNAME=1'
+FEATURE_FLAGS += -D'HAVE_UTSNAME=1'
 endif
 
 ifeq ($(HAS_CLOCK_GETTIME), yes)
-FEATURE_CFLAGS += -D'HAVE_CLOCK_GETTIME=1'
+FEATURE_FLAGS += -D'HAVE_CLOCK_GETTIME=1'
 override LDFLAGS += -lrt
 endif
 
@@ -943,7 +943,7 @@ config:
 	fi
 
 %.o: %.c | config
-	$(CC) -MMD $(CFLAGS) $(CPPFLAGS) $(FLASHROM_CFLAGS) $(FEATURE_CFLAGS) $(SCMDEF) -o $@ -c $<
+	$(CC) -MMD $(CFLAGS) $(CPPFLAGS) $(FLASHROM_CFLAGS) $(FEATURE_FLAGS) $(SCMDEF) -o $@ -c $<
 
 $(PROGRAM)$(EXEC_SUFFIX): $(OBJS)
 	$(CC) -o $(PROGRAM)$(EXEC_SUFFIX) $(OBJS) $(LDFLAGS)