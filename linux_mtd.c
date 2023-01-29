flashrom/flashrom

programmer: Smoothen register_opaque_master() API
It was impossible to register a const struct opaque_master that would
point to dynamically allocated `data`. Fix that so that we won't
have to create more mutable globals.

BUG=b:185191942
TEST=builds

Change-Id: Id3adb4cf04ae04dbe87ddb96f30871cb5f7c8ff0
Signed-off-by: Anastasia Klimchuk <aklm@chromium.org>
Reviewed-on: https://review.coreboot.org/c/flashrom/+/54170
Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>
Reviewed-by: Nico Huber <nico.h@gmx.de>
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

linux_mtd.c
@@ -424,7 +424,7 @@ int linux_mtd_init(void)
 		goto linux_mtd_init_exit;
 	}
 
-	register_opaque_master(&programmer_linux_mtd);
+	register_opaque_master(&programmer_linux_mtd, NULL);
 
 	ret = 0;
 linux_mtd_init_exit:

SourceArcade/flashprog

Original-Reviewed-on: https://review.coreboot.org/c/flashrom/+/54170
Original-Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>
Original-Reviewed-by: Nico Huber <nico.h@gmx.de>
Reviewed-on: https://review.coreboot.org/c/flashrom-stable/+/72202

linux_mtd.c
@@ -426,7 +426,7 @@ static int linux_mtd_init(void)
 		goto linux_mtd_init_exit;
 	}
 
-	register_opaque_master(&linux_mtd_opaque_master);
+	register_opaque_master(&linux_mtd_opaque_master, NULL);
 
 	ret = 0;
 linux_mtd_init_exit: