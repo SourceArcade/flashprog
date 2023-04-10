7a480310de5f7ec35d2d2acc7b60ca1ff8296944
Nico Huber <nico.h@gmx.de>
2023-04-10 14:54:22 +0200

    test_build: Add simple sanity check for explicit erase (-E)
    Change-Id: I210bd45431d2fa67118ab357df1b946bc83a3235
    Signed-off-by: Nico Huber <nico.h@gmx.de>
    Reviewed-on: https://review.coreboot.org/c/flashrom-stable/+/74323
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- test_build.sh
+++ test_build.sh
@@ -22,3 +22,7 @@ cmp ${TEMP_DIR}/rand ${TEMP_DIR}/bak
 dd bs=128K count=1 </dev/urandom >${TEMP_DIR}/rand
 ${FLASHROM} -p dummy:emulate=M25P10.RES,image=${TEMP_DIR}/image -c M25P10 -w ${TEMP_DIR}/rand
 ${FLASHROM} -p dummy:emulate=M25P10.RES,image=${TEMP_DIR}/image -c M25P10 -v ${TEMP_DIR}/rand
+
+dd bs=128K count=1 </dev/zero | tr '\000' '\377' >${TEMP_DIR}/empty
+${FLASHROM} -p dummy:emulate=M25P10.RES,image=${TEMP_DIR}/image -c M25P10 -E
+${FLASHROM} -p dummy:emulate=M25P10.RES,image=${TEMP_DIR}/image -c M25P10 -v ${TEMP_DIR}/empty
