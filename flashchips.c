0d4354eee32c834a602f5bec05803bd36977cfaa
Angel Pons <th3fanbus@gmail.com>
2024-05-26 16:33:51 +0200

    flashchips: Add W25Q32JV-.M
    Tested by `cobra` on IRC, on a ThinkPad R500.
    
    Change-Id: If1bffe0f09802f136636035f0f4ed31c3e33a7c4
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.sourcearcade.org/c/flashprog/+/150
    Reviewed-by: Nico Huber <nico.h@gmx.de>
    Tested-by: Nico Huber <nico.h@gmx.de>

--- flashchips.c
+++ flashchips.c
@@ -18458,6 +18458,61 @@ const struct flashchip flashchips[] = {
 
 	{
 		.vendor		= "Winbond",
+		.name		= "W25Q32JV-.M",
+		.bustype	= BUS_SPI,
+		.manufacture_id	= WINBOND_NEX_ID,
+		.model_id	= WINBOND_NEX_W25Q32JV,
+		.total_size	= 4096,
+		.page_size	= 256,
+		/* supports SFDP */
+		/* QPI enable 0x38 */
+		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_QPI |
+				  FEATURE_WRSR_EXT2 | FEATURE_WRSR2 | FEATURE_WRSR3,
+		.tested		= TEST_OK_PREW,
+		.probe		= probe_spi_rdid,
+		.probe_timing	= TIMING_ZERO,
+		.block_erasers	=
+		{
+			{
+				.eraseblocks = { {4 * 1024, 1024} },
+				.block_erase = spi_block_erase_20,
+			}, {
+				.eraseblocks = { {32 * 1024, 128} },
+				.block_erase = spi_block_erase_52,
+			}, {
+				.eraseblocks = { {64 * 1024, 64} },
+				.block_erase = spi_block_erase_d8,
+			}, {
+				.eraseblocks = { {4 * 1024 * 1024, 1} },
+				.block_erase = spi_block_erase_60,
+			}, {
+				.eraseblocks = { {4 * 1024 * 1024, 1} },
+				.block_erase = spi_block_erase_c7,
+			}
+		},
+		.printlock	= spi_prettyprint_status_register_bp2_tb_bpl,
+		.unlock		= spi_disable_blockprotect_bp2_srwd,
+		.write		= spi_chip_write_256,
+		.read		= spi_chip_read,
+		.voltage	= {2700, 3600},
+		.reg_bits	=
+		{
+			.srp    = {STATUS1, 7, RW},
+			.srl    = {STATUS2, 0, RW},
+			.bp     = {{STATUS1, 2, RW}, {STATUS1, 3, RW}, {STATUS1, 4, RW}},
+			.tb     = {STATUS1, 5, RW},
+			.sec    = {STATUS1, 6, RW},
+			.cmp    = {STATUS2, 6, RW},
+			.wps    = {STATUS3, 2, RW},
+		},
+		.wp_write_cfg	= spi_wp_write_cfg,
+		.wp_read_cfg	= spi_wp_read_cfg,
+		.wp_get_ranges	= spi_wp_get_available_ranges,
+		.decode_range	= decode_range_spi25,
+	},
+
+	{
+		.vendor		= "Winbond",
 		.name		= "W25Q32BW/W25Q32CW/W25Q32DW",
 		.bustype	= BUS_SPI,
 		.manufacture_id	= WINBOND_NEX_ID,
