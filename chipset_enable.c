3b9f152b4d53eef7f10f82cca6e808a223ac11e8
Nico Huber <nico.h@gmx.de>
2024-07-28 16:09:29 +0200

    chipset_enable: Probe AMD SPIBAR first and bail on ff
    Testing on a ThinkPad T14s Gen3 has shown that the LPC device's
    PCI config space can return all `ff' for everything from offset
    0x44 on. Reasons are unknown at this point, so it seems best to
    check for this and bail out.
    
    Change-Id: I92dcbdc2eb31652faaad1dba3ad6978de0c8024b
    Signed-off-by: Nico Huber <nico.h@gmx.de>
    Reviewed-on: https://review.sourcearcade.org/c/flashprog/+/200

--- chipset_enable.c
+++ chipset_enable.c
@@ -1436,14 +1436,12 @@ static int enable_flash_amd_spi100(struct flashprog_programmer *prog, struct pci
 		return ERROR_FATAL;
 	}
 
-	/* RomRange2 is supposed to be used for the mapping directly below 4G. */
-	const uint32_t rom_range2 = pci_read_long(lpc, 0x6c);
-	const uint32_t rom_range_end = rom_range2 | 0xffff;
-	const uint32_t rom_range_start = (rom_range2 & 0xffff) << 16;
-	const size_t mapped_len = rom_range_end > rom_range_start ? rom_range_end - rom_range_start + 1 : 0;
-	msg_pdbg("ROM Range 2: 0x%08x..0x%08x (%zu kB)\n", rom_range_start, rom_range_end, mapped_len / KiB);
-
 	const uint32_t spibar = pci_read_long(lpc, 0xa0);
+	if (spibar == 0xffffffff) {
+		msg_perr("SPI100 BAR reads all `ff', aborting.\n");
+		return ERROR_FATAL;
+	}
+
 	msg_pdbg("AltSpiCSEnable=%u, SpiRomEnable=%u", spibar >> 0 & 1, spibar >> 1 & 1);
 	msg_pdbg(", AbortEnable=%u, RouteTpm2Spi=%u", spibar >> 2 & 1, spibar >> 3 & 1);
 	msg_pdbg(", PspSpiMmioSel=%u\n", spibar >> 4 & 1);
@@ -1467,6 +1465,13 @@ static int enable_flash_amd_spi100(struct flashprog_programmer *prog, struct pci
 	void *const virt_spibar = rphysmap("SPI100 SPI registers", phys_spibar, 256);
 	if (virt_spibar == ERROR_PTR)
 		return ERROR_FATAL;
+
+	/* RomRange2 is supposed to be used for the mapping directly below 4G. */
+	const uint32_t rom_range2 = pci_read_long(lpc, 0x6c);
+	const uint32_t rom_range_end = rom_range2 | 0xffff;
+	const uint32_t rom_range_start = (rom_range2 & 0xffff) << 16;
+	const size_t mapped_len = rom_range_end > rom_range_start ? rom_range_end - rom_range_start + 1 : 0;
+	msg_pdbg("ROM Range 2: 0x%08x..0x%08x (%zu kB)\n", rom_range_start, rom_range_end, mapped_len / KiB);
 
 	void *memory_mapping = NULL;
 	if (spirom_enable && mapped_len) {
